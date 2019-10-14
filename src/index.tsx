abda25676a05db8cabc9dae781ad4a6e89c60ef2
Zhenan Hong <zhong@pebblepost.com>
2019-10-14 15:56:04 -0400

    init setup with Select and Button component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as global from './shared/global';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export {global};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export * from './components/Button/Button';
+export * from './components/Select/Select';
